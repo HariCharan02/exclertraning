e076334c83773497df406b88b5eb6d7034a0fc22
HariCharan02 <thumanapllyharicharan@gmail.com>
2025-01-27 09:37:40 +0530

    Add files via upload

---
+++ demo006.java
@@ -0,0 +1,10 @@
+public class demo006 {
+        public static void main(String[] args) {
+            String name = "Hari Charan";
+            String pwd = "Charan_Kanna02";
+            while ((name == "Hari Charan") && (pwd == "Charan_Kanna02")){
+                System.out.println("Hello Hari Charan welcome to Malla Reddy University");
+                break;
+            }
+        }
+    }
